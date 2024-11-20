efd835e8d1d9f69e220d328f63688efa34390d93
dian2174 <77834546+dian2174@users.noreply.github.com>
2024-11-20 12:18:01 +0100

    SceneManager

---
+++ P1-gr7/Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+
+    public static ScenesManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public enum Scene
+    {
+        StartScreen,
+        MainMenu,
+
+    }
+
+    public void LoadScene(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+
+    public void LoadNewGame()
+    {
+        SceneManager.LoadScene(Scene.StartScreen.ToString());
+
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(Scene.MainMenu.ToString());
+    }
+
+
+    
+}
