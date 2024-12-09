d25c1ce425a688c2131e48ebe9d94ed8f3168d51
Calcium1000 <cehh35@gmail.com>
2024-12-09 10:19:39 +0100

    small changes to FinishLine script

--- P1-gr7/Assets/Scripts/FinishLine.cs
+++ P1-gr7/Assets/Scripts/FinishLine.cs
@@ -7,7 +7,10 @@ public class FinishLine : MonoBehaviour
     public GhostHolder ghostHolder;
     //If working on implementing ghost play on 2 levels: Create two ghostHolders
 
-
+    private void NewRecord() //Displays new record time
+    {
+        Debug.Log($"NEW RECORD! Time is {ghostHolder.timeStamp[^1]} !");
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
 
@@ -16,14 +19,15 @@ public class FinishLine : MonoBehaviour
             Debug.Log("Player crossed the finish line!");
             scoreManager.instance.ShowWinScreen(); // Show the Win Screen
 
-            if(ghostHolder.recordTimeStamp.Count == 0)
+            if(ghostHolder.recordTimeStamp.Count == 0) //If no record time yet, add the new time as record
             {
                 ghostHolder.UpdateRecord();
+                NewRecord();
             }
             else if (ghostHolder.timeStamp[^1] < ghostHolder.recordTimeStamp[^1]) //Updates the record for the ghost player if new run time is better than old record
             {
                 ghostHolder.UpdateRecord();
-                Debug.Log("NEW RECORD! Time is " + ghostHolder.timeStamp[^1] + "!");
+                NewRecord();
             }
         }
     }
